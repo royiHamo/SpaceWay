5fed11dad47da621709985cb2536a4892a2d013b
moragjonathan <moragjonathan@BMW05>
2019-08-07 20:30:47 +0300

    Added Google and Facebook API

--- SpaceWay/Migrations/201908071129048_init.Designer.cs
+++ SpaceWay/Migrations/201908071129048_init.Designer.cs
@@ -7,13 +7,13 @@ namespace SpaceWay.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201906231936520_initial"; }
+            get { return "201908071129048_init"; }
         }
         
         string IMigrationMetadata.Source
