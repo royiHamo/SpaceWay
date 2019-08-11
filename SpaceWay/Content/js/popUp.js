7b2992f30767aa497bb1d9781a3cf70b19954c26
Mittelman <michal@mittelman.co.il>
2019-08-11 11:58:22 +0300

    PopUp in Search Flight

---
+++ SpaceWay/Content/js/popUp.js
@@ -0,0 +1,24 @@
+﻿//happens only if ViewBag.FaveStation is not empty
+function popUp() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    $(document).ready(function () {
+        modal.style.display = "block";
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
