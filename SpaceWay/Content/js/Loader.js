3498028da75903d2102050cadad94b0d1fa7ff39
Mittelman <michal@mittelman.co.il>
2019-08-12 11:34:41 +0300

    update loader and required in search flight

--- SpaceWay/Content/js/Loader.js
+++ SpaceWay/Content/js/Loader.js
@@ -1,6 +1,7 @@
 ﻿function load() {
     //show the loader div
     document.getElementById('loader').style.visibility = 'visible';
-    //load for 5 seconds and the submit the form
-    setTimeout(function () { $("#MyForm").submit(); }, 4000);
+    //load for 3 seconds and the submit the form, if not valid show error
+    setTimeout(function () { $("#realS").click(); document.getElementById('loader').style.visibility = 'hidden';}, 3000);
+   
 };
