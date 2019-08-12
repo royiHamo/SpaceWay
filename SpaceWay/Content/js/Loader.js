2e8593b0e9a0a8669356f81811c46bdccb62cc77
Mittelman <michal@mittelman.co.il>
2019-08-12 12:20:13 +0300

    loader and statistics

--- SpaceWay/Content/js/Loader.js
+++ SpaceWay/Content/js/Loader.js
@@ -3,5 +3,4 @@
     document.getElementById('loader').style.visibility = 'visible';
     //load for 3 seconds and the submit the form, if not valid show error
     setTimeout(function () { $("#realS").click(); document.getElementById('loader').style.visibility = 'hidden';}, 3000);
-   
 };
